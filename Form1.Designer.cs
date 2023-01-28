0ac82fb640882c9c74263f22b9333d427e63eb33
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-01-28 10:31:30 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace TextConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(122, 23);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(332, 294);
+            this.txtSource.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 23);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(93, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Choose File(s)";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(244, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 360);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtSource);
+            this.Name = "Form1";
+            this.Text = "TextConverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TextConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "SRT Files|*.srt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    string textWithoutTimestamps = RemoveTimestamps(file);
+                    SaveFile(textWithoutTimestamps, file);
+                }
+            }
+        }
+
+        private string RemoveTimestamps(string file)
+        {
+            string text = File.ReadAllText(file);
+            string textWithoutTimestamps = Regex.Replace(text, @"(\d{2}:\d{2}:\d{2}[,.]\d{3}\s-->\s\d{2}:\d{2}:\d{2}[,.]\d{3}\s)", "");
+            return textWithoutTimestamps;
+        }
+
+        private void SaveFile(string text, string file)
+        {
+            string newFile = Path.ChangeExtension(file, ".txt");
+            File.WriteAllText(newFile, text);
+        }
+    }
+}
